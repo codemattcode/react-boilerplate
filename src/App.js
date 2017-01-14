f941795de8dcdefc26341a5654b5b81c181d5873
codemattcode <codemattcode@gmail.com>
2017-01-14 02:42:58 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-wrapper">
+
+        <div className="app-header">
+          <h2>Best Web App Ever!</h2>
+        </div>
+        <p className="app-intro-txt">
+           <code>Start here with your code and magic</code>
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
